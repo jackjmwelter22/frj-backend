1ce5ca3d1ad3f4e9a675021818ddb254d36b95cb
Jack Welter <jackjmwelter22@Jacks-MacBook-Pro.local>
2021-09-22 21:22:06 -0400

    added more to register

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json({
-    type: ['application/json','text/plain']
+    type: ['application/json', 'text/plain']
 }))
 
 const {Client} = require('pg')
@@ -23,19 +25,34 @@ app.get('/', (req, res) => {
     res.send('Well done!');
 })
 
-app.post('/login',(req,res) => {
+app.post('/login', (req, res) => {
     console.log('Got body:', req.body);
     res.sendStatus(200);
 })
 
-app.post('/register',(req,res) => {
+app.post('/register', async (req, res) => {
     console.log('Got body:', req.body);
-    client.query(`INSERT INTO public.users ("email", "firstName", "lastName", "password") 
-VALUES ('${req.body.email}', '${req.body.firstName}', '${req.body.lastName}', '${req.body.password}')`)
-    res.sendStatus(200);
+    await client.query(`INSERT INTO public.users ("email", "firstName", "lastName", "password") 
+         VALUES ('${req.body.email}', '${req.body.firstName}', '${req.body.lastName}', '${req.body.password}')`,(error,results)=>{
+        if(error){
+            if(error.code=='23505') {
+                return res.send('Email already exists')
+            }
+            else{
+                return res.send('Please check all fields')
+            }
+        }
+        else{
+            return res.send('Account created successfully!')
+        }
+    })
 })
 
 app.listen(port, () => {
-    console.log('The application is listening on port 3000!');
+    console.log('The application is listening on port ' + port);
 })
+
+function isEmailValid(email) {
+
+}
 
